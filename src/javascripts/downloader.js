b363d4384bbf82afd6d5f7621903b9d9b48c6ebf
Denis <denisrobcizma@gmail.com>
2022-10-03 17:24:57 +0200

    Clean code

--- src/javascripts/downloader.js
+++ src/javascripts/downloader.js
@@ -47,7 +47,7 @@ buttonDownload.addEventListener('click', function (event) {
             fs.mkdirSync(savePath);
         }
     }
-    //Check if youtube url is valid ytdl
+    
     urlStat.innerText = 'Downloading...'
     var url = document.getElementById('input-url').value
     if (ytdl.validateURL(url)) {
@@ -124,18 +124,12 @@ function convertVideo(url, savePath,) {
             
         }
     })
-    // .catch (err => {
-    //     console.log(err)
-    // })
+    
 }
 
 //Download playlist
 function convertPlaylist(url, savePath) {
-    //Validate playlist url
-    // if (!ytpl.validateURL(url)){
-    //     urlStat.innerText = 'Invalid playlist URL'
-    //     return
-    // }
+    
     
     ytpl(url).then(playlist => {
         const format = document.getElementById('sel-format').value
@@ -201,115 +195,5 @@ function convertPlaylist(url, savePath) {
     })
 }
 
-// //If ipcRenderer doesnt get data then set defaultPath
-// ipcRenderer.on('selected-dir', (event, path) => {
 
-//     var savePath = path[0] + '/'
-
-    
-//     urlStat.innerText = 'Videos will be downloaded in: ' + savePath
-
-
-//     buttonDownload.addEventListener('click', function (event) {
-        
-//         //Save url and selected format to variable
-//         const url = document.getElementById('input-url').value
-//         const format = document.getElementById('sel-format').value
-
-//         //Check if URL exists
-//         if (ytdl.validateURL(url) == false) {
-//             document.getElementById('url-stat').innerText = "Cannot get video. Invalid URL."
-//             return;
-//         }else{
-//             urlStat.innerText = "Downloading...It can take a while"
-//         }
-
-        
-
-
-//         function saveDownload(videoURL){
-//             ytdl.getBasicInfo(videoURL).then(info => {
-//                 const title = info.videoDetails.title
-//                 const titleFixed = title.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '')
-//                 //Replace special chars
-//                 console.log(titleFixed)
-//                 if (format === 'mp3'){
-//                     const audioReadableStream = ytdl(videoURL, { quality: 'highestaudio', filter: 'audioonly' })
-                    
-//                     Ffmpeg(audioReadableStream)
-//                     .save(savePath + titleFixed + '.mp3')
-//                     .on("end", () => {
-//                         urlStat.innerText = "Downloaded " + title
-//                     })
-                    
-                    
-//                 } else {
-//                     //37=1080p, 22=720p, 18=360p
-//                     //Get all itags
-//                     var allItags = []
-//                     for (var i = 0; i < info.formats.length; i++) {
-//                         allItags.push(info.formats[i].itag)
-//                     }
-        
-//                     var desiredItags = [37, 22, 18]
-//                     var availableItags = []
-//                     for (var i = 0; i < desiredItags.length; i++) {
-//                         if (allItags.includes(desiredItags[i])) {
-//                             availableItags.push(desiredItags[i])
-//                         }
-//                     }
-//                     //Choose highest quality itag
-//                     var itag = ''
-//                     if (availableItags.includes(37)) {
-//                         itag = 37
-//                     } else if (availableItags.includes(22)) {
-//                         itag = 22
-//                     } else if (availableItags.includes(18)) {
-//                         itag = 18
-//                     }
-                    
-//                     const videoReadableStream = ytdl(videoURL, { quality: itag, filter:'audioandvideo' })
-//                     const writeStream = fs.createWriteStream(savePath + titleFixed + '.mp4')
-//                     videoReadableStream.pipe(writeStream)
-//                     writeStream
-//                     .on("end", () => {
-//                         urlStat.innerText = "Downloaded " + title
-//                     })
-//                 }
-//             })
-//         }
-
-//         //Check if playlist checkbox is checked
-//         if (document.getElementById('checkbox-playlist').checked) {
-
-//             // Get playlist ID and validate
-            // const playlistId = ytpl.getPlaylistID(url)
-            
-            // playlistId.then(result => {
-
-                
-            //     if (ytpl.validateID(result) == false) {
-            //         urlStat.innerText = "Invalid playlist"
-            //         return
-            //     } else {
-            //         urlStat.innerText = ""
-            //         //Loop trough every video in playlist
-            //         ytpl(result, {limit: Infinity, pages: Infinity}).then(result => {
-            //             for (var i = 0; i < result.items.length; i++) {
-            //                 var videoID = result.items[i].id
-            //                 var videoURL = 'https://www.youtube.com/watch?v=' + videoID
-            //                 saveDownload(videoURL)
-            //             }
-            //         })
-            //     }
-                
-//             })
-            
-//         } else{
-//             var videoURL = url
-//             saveDownload(videoURL)
-//             urlStat.innerText = "Downloaded succesfuly in " + savePath
-//         }
-//     });
-// })
 
